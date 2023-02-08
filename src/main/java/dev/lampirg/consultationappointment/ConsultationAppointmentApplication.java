56c0e2c8d8906a2b9be4d194f34d519d50599733
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-02-08 18:38:20 +0300

    Initial commit

---
+++ src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
@@ -0,0 +1,13 @@
+package dev.lampirg.consultationappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultationAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lampirg.consultationappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
