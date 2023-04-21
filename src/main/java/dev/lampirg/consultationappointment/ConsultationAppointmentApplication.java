f3bde3b5be7e542e63fc59e9c4d2290953693fdd
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-04-21 16:17:20 +0300

    move preloaded data
    into its own configuration PreLoadedData class

--- src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
+++ src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @SpringBootApplication
 public class ConsultationAppointmentApplication {
@@ -18,19 +19,4 @@ public class ConsultationAppointmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultationAppointmentApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner dataLoader(StudentRepository studentRepository, TeacherRepository teacherRepository) {
-		return args -> {
-			studentRepository.save(new Student("Василий", "Сакович", "Владиславович", "lampirg1@gmail.com", "ИВБ-911"));
-			studentRepository.save(new Student("Георгий", "Васильев", "Анатольевич", "georgii_vas@mail.ru", "КИБ-113"));
-			Teacher teacher = new Teacher();
-			teacher.setFirstName("Сергей");
-			teacher.setLastName("Иванов");
-			teacher.setPatronymic("Александрович");
-			teacher.getDatePeriod().add(new DatePeriod(LocalDateTime.of(2020, 5, 23, 12, 15),
-					LocalDateTime.of(2020, 5, 23, 12, 15)));
-		};
-	}
-
 }
