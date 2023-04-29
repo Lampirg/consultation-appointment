2dbbaa24bb1fa09e8c272fff8811a180e0bffc16
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-04-29 15:57:43 +0300

    Update TeacherRepository.java
    Add new method (find datePeriod by its id).

--- src/main/java/dev/lampirg/consultationappointment/data/teacher/TeacherRepository.java
+++ src/main/java/dev/lampirg/consultationappointment/data/teacher/TeacherRepository.java
@@ -3,9 +3,15 @@ package dev.lampirg.consultationappointment.data.teacher;
 import dev.lampirg.consultationappointment.data.model.PersonRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TeacherRepository extends PersonRepository<Teacher> {
+
     Page<Teacher> findByLastNameStartingWith(String lastName, Pageable pageable);
+
+    @Query("select t from DatePeriod t where t.id = ?1")
+    Optional<DatePeriod> findDatePeriodById(Long datePeriodId);
+
 }
