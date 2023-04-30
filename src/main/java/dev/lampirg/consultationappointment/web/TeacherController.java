7bc3b867d8d430e9008d49cee37c3d00469b6eee
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-04-30 09:34:48 +0300

    Update TeacherController.java
    Update openAddConsultationPage method.
    Add post addConsultation method.
    Add AppointmentMaker field.

--- src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
+++ src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
@@ -1,28 +1,43 @@
 package dev.lampirg.consultationappointment.web;
 
+import dev.lampirg.consultationappointment.data.appointment.Appointment;
+import dev.lampirg.consultationappointment.data.student.Student;
+import dev.lampirg.consultationappointment.data.teacher.DatePeriod;
 import dev.lampirg.consultationappointment.data.teacher.Teacher;
 import dev.lampirg.consultationappointment.data.teacher.TeacherRepository;
+import dev.lampirg.consultationappointment.service.AppointmentMaker;
+import dev.lampirg.consultationappointment.service.SimpleAppointmentMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
+@PreAuthorize("hasRole('STUDENT')")
 public class TeacherController {
 
-    TeacherRepository teacherRepository;
+    private TeacherRepository teacherRepository;
+    private AppointmentMaker appointmentMaker;
 
     @Autowired
-    public TeacherController(TeacherRepository teacherRepository) {
+    public TeacherController(TeacherRepository teacherRepository, SimpleAppointmentMaker appointmentMaker) {
         this.teacherRepository = teacherRepository;
+        this.appointmentMaker = appointmentMaker;
+    }
 
     @ModelAttribute("teacher")
     public Teacher findTeacher(@PathVariable(name = "id", required = false) Integer id) {
@@ -48,9 +63,20 @@ public class TeacherController {
         ModelAndView modelAndView = new ModelAndView("add-consultation");
         Teacher teacher = teacherRepository.findById(id)
                 .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+        Set<DatePeriod> datePeriods = teacher.getDatePeriods().stream()
+                .filter(datePeriod -> appointmentMaker.isAvailable(teacher, student, datePeriod))
+                .collect(Collectors.toSet());
+        modelAndView.addObject("datePeriods", datePeriods);
         modelAndView.addObject(teacher);
         return modelAndView;
     }
+
+    @PostMapping("/teachers/{id}/add")
+    public String addConsultation(@PathVariable("id") int id, Long datePeriodId,
+                                  Teacher teacher, @AuthenticationPrincipal Student student) {
+        DatePeriod datePeriod = teacherRepository.findDatePeriodById(datePeriodId).orElseThrow();
+        appointmentMaker.makeAppointment(teacher, student, datePeriod);
+        return "redirect:/teachers/" + id;
     }
 
     @GetMapping
@@ -75,13 +101,11 @@ public class TeacherController {
             // no owners found
             result.rejectValue("lastName", "notFound", "Преподаватель не найден");
             return "find-teacher";
-        }
-        else if (ownersResults.getTotalElements() == 1) {
+        } else if (ownersResults.getTotalElements() == 1) {
             // 1 teacher found
             teacher = ownersResults.iterator().next();
             return "redirect:/teachers/" + teacher.getId();
-        }
-        else {
+        } else {
             // multiple owners found
             return addPaginationModel(page, model, ownersResults);
         }
