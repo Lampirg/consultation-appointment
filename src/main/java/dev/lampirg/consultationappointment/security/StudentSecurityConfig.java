ff3fb354a597bae25d719ead4bd8b706b438abd2
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-01 20:29:06 +0300

    split SecurityConfig
    Into StudentSecurityConfig and TeacherSecurityConfig

--- src/main/java/dev/lampirg/consultationappointment/security/StudentSecurityConfig.java
+++ src/main/java/dev/lampirg/consultationappointment/security/StudentSecurityConfig.java
@@ -4,29 +4,26 @@ import dev.lampirg.consultationappointment.data.student.StudentRepository;
 import dev.lampirg.consultationappointment.data.teacher.TeacherRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
-public class SecurityConfig {
+@EnableWebSecurity
+public class StudentSecurityConfig {
 
-    // TODO: split methods for students and teachers into different classes
+    PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public StudentSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Bean
@@ -34,34 +31,24 @@ public class SecurityConfig {
         return new PersonDetailService<>(studentRepository);
     }
 
-    @Bean
-    public UserDetailsService teacherDetailService(TeacherRepository teacherRepository) {
-        return new PersonDetailService<>(teacherRepository);
-    }
 
     @Bean
     public AuthenticationProvider studentAuthenticationProvider(UserDetailsService studentDetailService) throws Exception {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(studentDetailService);
-        authenticationProvider.setPasswordEncoder(encoder());
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
     }
 
-    @Bean
-    public AuthenticationProvider teacherAuthenticationProvider(UserDetailsService teacherDetailService) throws Exception {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(teacherDetailService);
-        authenticationProvider.setPasswordEncoder(encoder());
-        return authenticationProvider;
-    }
 
     @Bean
     public SecurityFilterChain studentFilterChain(HttpSecurity http, AuthenticationProvider studentAuthenticationProvider) throws Exception {
         // @formatter:off
         http
+                .securityMatcher("/student/**")
                 .authenticationProvider(studentAuthenticationProvider)
                 .authorizeHttpRequests()
-                    .anyRequest().hasRole("'USER'")
+                    .requestMatchers("/student/**").hasRole("STUDENT")
                     .and()
                 .formLogin()
                     .loginPage("/student/login")
@@ -78,25 +65,6 @@ public class SecurityConfig {
         // @formatter:on
         return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain teacherFilterChain(HttpSecurity http, AuthenticationProvider teacherAuthenticationProvider) throws Exception {
-//        http
-//                .authenticationProvider(teacherAuthenticationProvider)
-//                .authorizeHttpRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .usernameParameter("email")
-//                .loginPage("/teacher/login")
-//                .loginProcessingUrl("/teacher/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/teacher/logout")
-//                .and().httpBasic();
-//        return http.build();
-//    }
 
 
 }
