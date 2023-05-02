04297fb1c821a9254e0ffcfe8e72a6638f947f3e
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-02 08:20:10 +0300

    Update TeacherController.java
    Finish getTeacherProfile method.

--- src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
+++ src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
@@ -1,5 +1,6 @@
 package dev.lampirg.consultationappointment.web;
 
+import dev.lampirg.consultationappointment.data.teacher.DatePeriod;
 import dev.lampirg.consultationappointment.data.teacher.Teacher;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,6 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
@@ -15,6 +20,9 @@ public class TeacherController {
     @GetMapping("/profile")
     public String getTeacherProfile(@AuthenticationPrincipal Teacher teacher, Model model) {
         model.addAttribute("teacher", teacher);
+        List<DatePeriod> datePeriods = new ArrayList<>(teacher.getDatePeriods());
+        datePeriods.sort(Comparator.comparing(DatePeriod::getStartTime));
+        model.addAttribute("consultations", datePeriods);
         return "teacher/teacher-profile";
     }
 
