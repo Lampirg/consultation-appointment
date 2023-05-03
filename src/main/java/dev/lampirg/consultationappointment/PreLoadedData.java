ff3088fac68afffe2f75d67ac7fdae026895dc60
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-03 10:57:24 +0300

    Update PreLoadedData.java
    Change data upload.
    Add @Profile("dev").

--- src/main/java/dev/lampirg/consultationappointment/PreLoadedData.java
+++ src/main/java/dev/lampirg/consultationappointment/PreLoadedData.java
@@ -1,5 +1,6 @@
 package dev.lampirg.consultationappointment;
 
+import dev.lampirg.consultationappointment.data.appointment.AppointmentRepository;
 import dev.lampirg.consultationappointment.data.student.Student;
 import dev.lampirg.consultationappointment.data.student.StudentRepository;
 import dev.lampirg.consultationappointment.data.teacher.DatePeriod;
@@ -8,35 +9,48 @@ import dev.lampirg.consultationappointment.data.teacher.TeacherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Configuration
+@Profile("dev")
 public class PreLoadedData  {
 
     StudentRepository studentRepository;
     TeacherRepository teacherRepository;
+    AppointmentRepository appointmentRepository;
 
-    public PreLoadedData(StudentRepository studentRepository, TeacherRepository teacherRepository) {
+    public PreLoadedData(StudentRepository studentRepository, TeacherRepository teacherRepository, AppointmentRepository appointmentRepository) {
         this.studentRepository = studentRepository;
         this.teacherRepository = teacherRepository;
+        this.appointmentRepository = appointmentRepository;
     }
 
     @Bean
     public CommandLineRunner dataLoader() throws Exception {
         return args -> {
             studentRepository.save(new Student("Василий", "Сакович", "Владиславович", "vasilii@gmail.com", "{noop}qwerty", "ИВБ-911"));
-            studentRepository.save(new Student("Георгий", "Васильев", "Анатольевич", "georgii_vas@mail.ru", "{noop}qwerty", "КИБ-113"));
+            Student student = studentRepository.save(new Student("Георгий", "Васильев", "Анатольевич", "georgii_vas@mail.ru", "{noop}qwerty", "КИБ-113"));
             Teacher teacher = new Teacher("Сергей", "Иванов", "Александрович", "ivanov@pgups.ru", "{noop}qwerty");
-            teacher.getDatePeriods().add(new DatePeriod(LocalDateTime.of(2023, 5, 23, 12, 15),
-                    LocalDateTime.of(2023, 5, 23, 14, 15)));
+            teacher.getDatePeriods().add(new DatePeriod(
+                    "7-421",
+                    LocalDateTime.of(2023, 5, 23, 12, 15),
+                    LocalDateTime.of(2023, 5, 23, 14, 15))
+            );
             teacherRepository.save(teacher);
             teacher = new Teacher("Александр", "Щёлоков", "Анатольевич", "vasilii@gmail.com", "{noop}qwerty");
-            teacher.getDatePeriods().add(new DatePeriod(LocalDateTime.of(2023, 5, 11, 11, 15),
-                    LocalDateTime.of(2023, 5, 11, 12, 15)));
-            teacher.getDatePeriods().add(new DatePeriod(LocalDateTime.of(2023, 5, 15, 11, 15),
-                    LocalDateTime.of(2023, 5, 15, 12, 15)));
+            teacher.getDatePeriods().add(new DatePeriod(
+                    "1-204",
+                    LocalDateTime.of(2023, 5, 11, 11, 15),
+                    LocalDateTime.of(2023, 5, 11, 12, 15))
+            );
+            teacher.getDatePeriods().add(new DatePeriod(
+                    "4-301",
+                    LocalDateTime.of(2023, 5, 15, 11, 15),
+                    LocalDateTime.of(2023, 5, 15, 12, 15))
+            );
             teacherRepository.save(teacher);
             for (int i = 0; i < 15; i++) {
                 String firstName = UUID.randomUUID().toString().substring(0, 5);
