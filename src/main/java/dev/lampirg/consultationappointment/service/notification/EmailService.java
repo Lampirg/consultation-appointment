f9a0bed4f865c503a7639e0b62d9d9bad2651c37
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-03 13:06:56 +0300

    Finish EmailService

--- src/main/java/dev/lampirg/consultationappointment/service/notification/EmailService.java
+++ src/main/java/dev/lampirg/consultationappointment/service/notification/EmailService.java
@@ -1,9 +1,8 @@
 package dev.lampirg.consultationappointment.service.notification;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.mail.MailMessage;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +11,11 @@ public class EmailService {
 
     private MailSender mailSender;
 
-    @Value("${spring.mail.host}")
-    private String host;
-
     public EmailService(MailSender mailSender) {
         this.mailSender = mailSender;
     }
     // TODO: this method
-    public void sendEmail(MailMessage message) {
-        System.out.println(message);
+    public void sendEmail(SimpleMailMessage message) {
+        mailSender.send(message);
     }
 }
