e74585e6d37c96c481326fe4dc63cfad70d4d6c3
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-03 15:24:49 +0300

    Enable @Async

--- src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
+++ src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
@@ -2,8 +2,10 @@ package dev.lampirg.consultationappointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ConsultationAppointmentApplication {
 
 	public static void main(String[] args) {
