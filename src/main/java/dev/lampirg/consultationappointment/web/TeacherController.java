2ad7aedee5b0d2ce4628a4bf0a32daa8181b8815
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-04 23:25:40 +0300

    Extract ConsultationInfo
    Into dev.lampirg.consultationappointment.web.fetch.

--- src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
+++ src/main/java/dev/lampirg/consultationappointment/web/TeacherController.java
@@ -4,8 +4,7 @@ import dev.lampirg.consultationappointment.data.teacher.DatePeriod;
 import dev.lampirg.consultationappointment.data.teacher.Teacher;
 import dev.lampirg.consultationappointment.data.teacher.TeacherRepository;
 import dev.lampirg.consultationappointment.service.teacher.ConsultationMaker;
-import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
+import dev.lampirg.consultationappointment.web.fetch.ConsultationInfo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -59,27 +56,15 @@ public class TeacherController {
     @PostMapping("/consultation/add")
     public String createConsultation(ConsultationInfo consultation, @AuthenticationPrincipal Teacher teacher) {
         DatePeriod datePeriod = new DatePeriod(
-                consultation.classroom,
-                LocalDateTime.of(consultation.date, consultation.startTime),
-                LocalDateTime.of(consultation.date, consultation.endTime)
+                consultation.getClassroom(),
+                LocalDateTime.of(consultation.getDate(), consultation.getStartTime()),
+                LocalDateTime.of(consultation.getDate(), consultation.getEndTime())
         );
         consultationMaker.createConsultation(teacherRepository.findById(teacher.getId()).orElseThrow(), datePeriod);
         return "redirect:/teacher/profile";
     }
 
     // TODO: methods for creating, displaying and deleting consultation patterns ("/pattern/add" and "/pattern")
-
-    @Data
-    public static class ConsultationInfo {
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-        private LocalDate date;
-
-        private String classroom;
-        @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-        private LocalTime startTime;
-        @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-        private LocalTime endTime;
-
     }
 
 }
