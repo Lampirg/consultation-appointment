0327598a0aba5c53e55046d6a426ff25017984c6
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-04 14:06:23 +0300

    Enable scheduling

--- src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
+++ src/main/java/dev/lampirg/consultationappointment/ConsultationAppointmentApplication.java
@@ -3,9 +3,11 @@ package dev.lampirg.consultationappointment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class ConsultationAppointmentApplication {
 
 	public static void main(String[] args) {
