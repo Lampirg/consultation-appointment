e2340253bab3e0e9edd95070a12369badfdfa859
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-09 15:37:04 +0300

    Update SchedulesRunner.java
    Schedule deletion of all created consultations.

--- src/main/java/dev/lampirg/consultationappointment/service/teacher/SchedulesRunner.java
+++ src/main/java/dev/lampirg/consultationappointment/service/teacher/SchedulesRunner.java
@@ -2,6 +2,8 @@ package dev.lampirg.consultationappointment.service.teacher;
 
 import dev.lampirg.consultationappointment.data.schedule.PatternSchedule;
 import dev.lampirg.consultationappointment.data.schedule.PatternScheduleRepository;
+import dev.lampirg.consultationappointment.data.teacher.Teacher;
+import dev.lampirg.consultationappointment.data.teacher.TeacherRepository;
 import dev.lampirg.consultationappointment.web.fetch.ConsultationPattern;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +17,14 @@ public class SchedulesRunner {
 
     private PatternScheduleRepository patternScheduleRepository;
     private ConsultationScheduler consultationScheduler;
+    private TeacherRepository teacherRepository;
+    private AutoDeleteConsultationAspect deleteConsultationAspect;
 
-    public SchedulesRunner(PatternScheduleRepository patternScheduleRepository, ConsultationScheduler consultationScheduler) {
+    public SchedulesRunner(PatternScheduleRepository patternScheduleRepository, ConsultationScheduler consultationScheduler, TeacherRepository teacherRepository, AutoDeleteConsultationAspect deleteConsultationAspect) {
         this.patternScheduleRepository = patternScheduleRepository;
         this.consultationScheduler = consultationScheduler;
+        this.teacherRepository = teacherRepository;
+        this.deleteConsultationAspect = deleteConsultationAspect;
     }
 
     @Bean
@@ -27,6 +33,8 @@ public class SchedulesRunner {
         return args -> {
             List<PatternSchedule> schedules = patternScheduleRepository.findAll();
             schedules.forEach((schedule) -> consultationScheduler.addPattern(ConsultationPattern.fromPatternSchedule(schedule)));
+            List<Teacher> teachers = teacherRepository.findAll();
+            teachers.forEach(teacher -> teacher.getDatePeriods().forEach(datePeriod -> deleteConsultationAspect.scheduleConsultationDeletion(teacher, datePeriod)));
         };
     }
 }
