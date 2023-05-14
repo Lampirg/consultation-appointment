c33692a65fd3db226b44421de9d00bad012e8a72
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-14 22:59:29 +0300

    Delete admin
    Won't be using this.

--- src/main/java/dev/lampirg/consultationappointment/data/admin/AdminRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.lampirg.consultationappointment.data.admin;
-
-import dev.lampirg.consultationappointment.data.model.PersonRepository;
-
-public interface AdminRepository extends PersonRepository<Admin> {
-}
