9c38dbaf90447cb1b7be3b87dd1214e4802dfa30
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-05-19 19:18:12 +0300

    Refactor: move to new configuration style

--- src/main/java/dev/lampirg/consultationappointment/security/TeacherSecurityConfig.java
+++ src/main/java/dev/lampirg/consultationappointment/security/TeacherSecurityConfig.java
@@ -39,27 +39,25 @@ public class TeacherSecurityConfig {
 
     @Bean
     public SecurityFilterChain teacherFilterChain(HttpSecurity http, AuthenticationProvider teacherAuthenticationProvider) throws Exception {
-        // @formatter:off
         http
                 .securityMatcher("/teacher/**")
                 .authenticationProvider(teacherAuthenticationProvider)
-                .authorizeHttpRequests()
-                    .requestMatchers("/teacher/**").hasRole("TEACHER")
-                    .and()
-                .formLogin()
-                    .loginPage("/teacher/login")
-                    .failureUrl("/teacher/login?hasError=true")
-                    .usernameParameter("email")
-                    .loginProcessingUrl("/teacher/login")
-                    .defaultSuccessUrl("/teacher/profile")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .logoutUrl("/teacher/logout")
-                    .logoutSuccessUrl("/home")
-                    .and()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/teacher/**").hasRole("TEACHER")
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/teacher/login")
+                        .failureUrl("/teacher/login?hasError=true")
+                        .usernameParameter("email")
+                        .loginProcessingUrl("/teacher/login")
+                        .defaultSuccessUrl("/teacher/profile")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/teacher/logout")
+                        .logoutSuccessUrl("/home")
+                )
                 .httpBasic();
-        // @formatter:on
         return http.build();
     }
 }
